13fb12a5dbcb808277544038540c7801df2871bb
jxxu <jxxu@taifinance.cn>
2023-06-17 18:32:37 +0800

    feat(修改环境)

--- constants/apiConfig.js
+++ constants/apiConfig.js
@@ -1,4 +1,4 @@
 // 生产
 // export const API_URL = "http://36.156.143.22:8090";
 // 测试
-export const API_URL = "http://www.hongyuedu.cn:8001/api";
+// export const API_URL = "http://www.hongyuedu.cn:8001/api";
